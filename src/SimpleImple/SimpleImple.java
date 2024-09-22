5d71b7983f8c0cf5d0b1fd31576ecb4c21928dd1
KateC08 <coming.katelyn08@gmail.com>
2024-09-22 13:41:30 +0800

    Imple

--- src/SimpleImple/SimpleImple.java
+++ src/SimpleImple/SimpleImple.java
@@ -12,4 +12,19 @@ package SimpleImple;
  */
 public class SimpleImple {
     
+    public static void main(String[] args) {
+    //Input
+        String[] names = {"Katelyn", "Katel", "keyt"};
+        
+        //Output
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println("~~~~~~~");
+        
+        //process to print all the values of the array
+        for(int i = 0; i < names.length; i++) {
+            System.out.print(names[i] + ", ");
+        }
+    }
 }
